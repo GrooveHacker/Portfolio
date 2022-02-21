7d2c003832e92556c5c78c335ee71ba300994014
GrooveHacker <81492261+GrooveHacker@users.noreply.github.com>
2022-02-21 13:35:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    document.querySelector("#button").onclick = () => {
+        $.ajax({
+            type: "POST",
+            url: "/login.php",
+            data: {"password": document.querySelector("#input").value},
+            success: (data) => {
+                console.log(data);
+            },
+            dataType: "json"
+        });
+    };
+});
